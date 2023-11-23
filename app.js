5b48772cfbac14e3155ac78abbd009e6caa05737
モト2318 <suigekka0101@gmail.com>
2023-11-23 22:32:56 +0900

    jqueryの入れ直し

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-let i;
-let num = 0;
-
-for(i = 1; i < 11; i++){
-  num = num + i;
-}
-
-alert('1から10まで足し算した結果は' + num + 'です');
+$(document).ready(function () {
+  $('body').html('<h1>Hello jQuery!!</h1>');
+});
