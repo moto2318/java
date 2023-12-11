0f4bef25143fc0b32bfce9f25b704a5b0f08a3b1
モト2318 <suigekka0101@gmail.com>
2023-12-11 22:45:53 +0900

    jsの変更・カーソル移動で色変化

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+// カーソルでクリックして色の変更
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+// });
+
+
+
+// カーソル合わせで色が変更する
 $(function(){
   $('.box1').mouseover(function(){
     $('.box1').css({'background-color': '#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#FF0000'});
   });
 });
